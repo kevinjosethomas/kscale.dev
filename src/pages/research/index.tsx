7aeb20c21e6e706793c20252aba1f1dd05e35db1
budzianowski <budzianowski@gmail.com>
2025-01-31 00:02:29 -0800

    save draft

--- src/pages/research/index.tsx
+++ src/pages/research/index.tsx
@@ -24,9 +24,9 @@ export async function getStaticProps() {
     };
   });
 
-  // Sort by oldest date first
+  // Sort by newest date first
   const sortedPosts = posts.sort((a, b) => {
-    return new Date(a.date).getTime() - new Date(b.date).getTime();
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
   });
 
   return { props: { posts: sortedPosts } };
